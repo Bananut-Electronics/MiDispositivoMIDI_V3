1986a6223ab56b399c59470a24f11b6fc0fed713
pabloEntropia <pablo.alonso@upf.edu>
2019-08-03 20:15:37 +0200

    Automelody improvement
    This update add the following functionalities:
    
    - PADs 0 to 3 generates lower notes -(I, III, IV, V)
    - PADs 4 to 7 generates higher notes +(I, III, IV, V)
    - PADs 8 to 9 are octave up/down buttons
    - PADs 10 to 11 are semitone up/down buttons
    - PADs 12 to 15 are switches for the scale. You can choose among:
    RUMBITA, BACH, PACO or  CHINESE!

--- examples/MDM_V3_Library_Automelody/MDM_V3_Library_Automelody.ino
+++ examples/MDM_V3_Library_Automelody/MDM_V3_Library_Automelody.ino
@@ -18,69 +18,241 @@
 // Include the library
 #include "MiDispositivoMIDI_V3.h"
 
+enum Scale {RUMBITA, BACH, PACO, CHINESE};
 
 class MiDispositivoMIDIAutomelody : public MiDispositivoMIDI_V3 {
 
-public: MiDispositivoMIDIAutomelody(uint8_t numPages, uint8_t numExtensions): MiDispositivoMIDI_V3(numPages, numExtensions) {}
+public: MiDispositivoMIDIAutomelody(uint8_t numPages, uint8_t numExtensions): MiDispositivoMIDI_V3(numPages, numExtensions)
+{
+    _transposition = 0;
+    _current_octave = _origin_octave;
+
+
+    setTransModel(BACH);
+    initializeLEDs();
+}
 
 protected:
     const uint8_t _origin_chroma = 0;
     const uint8_t _origin_octave = 5;
     
-    uint8_t _current_chroma = _origin_chroma;
-    uint8_t _current_octave = _origin_octave;
+    uint8_t _current_octave;
+    int16_t _transposition;
 
-    uint8_t _current_note = _current_chroma + _current_octave * 12;
+    uint8_t _current_note;
 
-    uint8_t _low_bound  = _current_note - 7;
-    uint8_t _high_bound = _current_note + 7;
+    int8_t _note_shift[8] = {-7, -4, -3, -1,
+                              7,  4,  3,  1};
 
-    uint8_t _note_shift[4]  = {-7, -5, 5, 5};
-    uint8_t _bound_shift[4] = {-7, 0, 0, 7};
+    uint8_t _COLOR_SCALE_ON[3] = {128, 0, 128};
+    uint8_t _COLOR_SCALE_OFF[3] = {20, 0, 20};
 
-    uint8_t _DEBUG_RED[3] = {1, 0, 0};
+    Scale _scale;
 
-    const float _trans_model1[12][12] = {
-    {0.71, 0.18, 0.00, 0.00, 0.00, 0.05, 0.00, 0.00, 0.40, 0.00, 0.09, 0.00},
-    {0.05, 0.50, 0.00, 0.07, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.09, 0.00},
-    {0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00},
-    {0.13, 0.05, 0.00, 0.77, 0.00, 0.10, 0.06, 0.00, 0.00, 0.00, 0.00, 0.00},
-    {0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00},
-    {0.03, 0.23, 0.00, 0.02, 0.00, 0.79, 0.00, 0.00, 0.00, 0.00, 0.02, 0.00},
-    {0.00, 0.00, 0.00, 0.00, 0.00, 0.05, 0.83, 0.00, 0.05, 0.00, 0.00, 0.00},
-    {0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00},
-    {0.00, 0.00, 0.00, 0.13, 0.00, 0.00, 0.11, 0.00, 0.50, 0.00, 0.00, 0.00},
-    {0.00, 0.00, 0.00, 0.02, 0.00, 0.00, 0.00, 0.00, 0.00, 0.50, 0.00, 0.00},
-    {0.08, 0.03, 0.00, 0.00, 0.00, 0.02, 0.00, 0.00, 0.05, 0.50, 0.81, 0.00},
-    {0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00}
+    const uint8_t (*_trans_model)[12];
+    const uint8_t *_chroma_probs;
+
+    const uint8_t _trans_model_1[12][12] = {
+        {71, 18, 0,  0, 0, 05,  0, 0, 40,  0,  9, 0},
+        { 5, 50, 0,  7, 0,  0,  0, 0,  0,  0,  9, 0},
+        { 0,  0, 0,  0, 0,  0,  0, 0,  0,  0,  0, 0},
+        {13,  5, 0, 77, 0, 10,  6, 0,  0,  0,  0, 0},
+        { 0,  0, 0,  0, 0,  0,  0, 0,  0,  0,  0, 0},
+        { 3, 23, 0,  2, 0, 79,  0, 0,  0,  0,  2, 0},
+        { 0,  0, 0,  0, 0,  5, 83, 0,  5,  0,  0, 0},
+        { 0,  0, 0,  0, 0,  0,  0, 0,  0,  0,  0, 0},
+        { 0,  0, 0, 13, 0,  0, 11, 0, 50,  0,  0, 0},
+        { 0,  0, 0,  2, 0,  0,  0, 0,  0, 50,  0, 0},
+        { 8,  3, 0,  0, 0,  2,  0, 0,  5, 50, 81, 0},
+        { 0,  0, 0,  0, 0,  0,  0, 0,  0,  0,  0, 0}
+        };
+
+    const uint8_t _chroma_probs_1[12] = {22, 8, 0, 22, 0, 15, 6, 0, 7, 0, 16, 0};
+
+    const uint8_t _trans_model_2[12][12] = {
+        {54,  6,  2,  2,  4,  9,  0,  4,  7,  1, 11,  0},
+        {13, 47,  0,  6,  7,  7,  1,  3,  2,  0, 14,  0},
+        { 8,  1, 48, 12,  9, 10,  1,  2,  1,  0,  9,  1},
+        { 7, 11,  3, 50,  0,  9,  2,  5,  7,  1,  5,  0},
+        {11,  8,  1,  0, 45, 13,  0, 18,  1,  0,  2,  0},
+        {11,  5,  1,  6,  4, 50,  2,  6,  6,  2,  7,  0},
+        { 1,  2,  1, 12,  1, 18, 49,  0, 10,  1,  6,  0},
+        { 8,  2,  0,  3, 10,  7,  0, 49, 10,  1, 11,  0},
+        {10,  3,  1,  3,  1, 11,  2,  9, 48,  0, 11,  0},
+        {10,  5,  0,  2,  0, 17,  3,  2,  2, 45, 14,  2},
+        {14,  6,  2,  3,  2,  5,  1,  9,  7,  2, 49,  0},
+        {13,  0,  7,  3,  0,  0,  0, 17,  3,  7,  7, 43}
+        };
+
+    const uint8_t _chroma_probs_2[12] = {19, 9, 2, 7, 7, 15, 2, 12, 1, 2, 15, 0};
+
+
+    const uint8_t _trans_model_3[12][12] = {
+        {69,  0, 0,  0,  0,  0,  0,  0,  0,  0,  0, 30},
+        { 4, 10, 0, 21,  0, 11,  2,  0, 28,  0,  3, 20},
+        { 0, 25, 2, 38,  0,  4,  0,  0,  0,  0, 21, 10},
+        { 0, 14, 7,  5,  0, 10,  4,  0, 43,  0, 14,  4},
+        { 0,  0, 0, 78,  0, 22,  0,  0,  0,  0,  0,  0},
+        { 0,  5, 1, 24,  2,  6, 14,  0,  6,  0, 40,  1},
+        { 0,  3, 0, 16,  0, 46,  6,  0, 15,  0,  6,  7},
+        { 0,  0, 0,  0,  0,  0, 17,  0, 67,  0, 17,  0},
+        { 0, 11, 0, 37,  0,  7, 18,  1,  2, 18,  5,  0},
+        { 1,  0, 0,  1,  0,  0,  1,  0,  1,  1, 88,  7},
+        { 1,  1, 0, 27,  0, 34,  7,  0, 10,  4,  6, 10},
+        {59,  4, 0,  1,  0,  2,  3,  0,  1,  0, 10, 21}};
+
+
+    const uint8_t _chroma_probs_3[12] = {31, 4, 1, 11, 0, 8, 5, 0, 9, 2, 10, 15};
+
+
+    const uint8_t _trans_model_4[12][12] = {
+        {19, 0,  44, 0, 3,   6, 0,   9, 0,  16,  3,  0},
+        { 0, 0,   0, 0, 0,   0, 0,   0, 0,   0,  0,  0},
+        {56, 0,  11, 1, 5,  18, 0,   3, 0,   5,  1,  1},
+        {22, 0,  33, 0, 0,  33, 0,  11, 0,   0,  0,  0},
+        {14, 0,  27, 0, 7,   4, 0,  38, 0,   7,  0,  4},
+        {22, 0,  20, 1, 5,  23, 0,  20, 0,   9,  1,  0},
+        { 0, 0,   0, 0, 0, 100, 0,   0, 0,   0,  0,  0},
+        {12, 0,  12, 0, 6,  12, 0,  17, 0,  39,  1,  1},
+        { 0, 0,   0, 0, 0,   0, 0, 100, 0,   0,  0,  0},
+        {28, 0,   7, 0, 3,  10, 0,  26, 0,  15, 11,  0},
+        {23, 0,   3, 0, 0,   1, 0,   1, 0,  72,  0,  0},
+        { 7, 0,   0, 0, 0,   0, 0,   0, 0,  79,  0, 14}};
+
+    const uint8_t _chroma_probs_4[12] = {27, 0, 20, 0, 4, 11, 0, 14, 0, 17, 2, 0};
+
+
+    void setTransModel(Scale scale) {
+        _scale = scale;
+
+        switch (_scale)
+        {
+        case RUMBITA:
+            _trans_model = _trans_model_1;
+            _chroma_probs = _chroma_probs_1;
+            break;
+
+        case BACH:
+            _trans_model = _trans_model_2;
+            _chroma_probs = _chroma_probs_2;
+            break;
+
+        case PACO:
+            _trans_model = _trans_model_3;
+            _chroma_probs = _chroma_probs_3;
+            break;
+
+        case CHINESE:
+            _trans_model = _trans_model_4;
+            _chroma_probs = _chroma_probs_4;
+            break;
+        
+        default:
+            break;
+        }
     };
 
-    const float _chroma_probs[12] = {0.083, 0.083, 0.083, 0.083,
-                                   0.083, 0.083, 0.083, 0.083,
-                                   0.083, 0.083, 0.083, 0.083};
+    void initializeLEDs()
+    {
+        // Note  pads
+        for(uint8_t i = 0; i < 8; i++)
+        {
+            for(uint8_t j = 0; j < _numberExtensions; j++)
+            {
+                setRgbColors(_offColors, _ledPad[j][i]);
+            }
+        }
+
+        // Octave PADs
+        updateOctaveLEDs();
+        updateTranspositionLEDs();
+
+        // Scale pads
+        for(uint8_t i = 12; i < 16; i++)
+        {
+            for(uint8_t j = 0; j < _numberExtensions; j++)
+            {
+                setRgbColors(_COLOR_SCALE_OFF, _ledPad[j][i]);
+            }
+        }
+        setRgbColors(_COLOR_SCALE_ON, _ledPad[0][12 + _scale]);
+    }
 
     void PADaction(PADstatus status, uint8_t extension, uint8_t pad)
     {
         if (status == ONSET)
         {
+            if (pad < 8)
+            {
+                setRgbColors(_onColors, _ledPad[extension][pad]);
+                updateNote(extension, pad);
+            }
+            else if (pad >= 8 && pad < 10)
+            {
+                /* Octave trainsposition */
+                if (pad == 8)
+                {
+                    _current_note += 12;
+                }
+                if (pad == 9)
+                {
+                    _current_note -= 12;
+                }
+                _current_octave = _current_note / 12;
 
-            setRgbColors(_onColors, _ledPad[extension][pad]);
-            updateNote(extension, pad);
+                updateOctaveLEDs();
+                return;
+            }
+            else if (pad >= 10 && pad < 12)
+            {
+                /* Semitone Transposition */
+                if (pad == 10)
+                {
+                    if (_transposition < 12)
+                    {
+                        _transposition++;
+                        _current_note++;
+                    }
+                }
+                if (pad == 11)
+                {
+                    if (_transposition > -12)
+                    {
+                        _transposition--;
+                        _current_note--;
+                    }
+                }
+                _current_octave = _current_note / 12;
+
+                updateTranspositionLEDs();
+                return;
+            }
+            else if (pad >= 12 && pad < 16)
+            {
+                /* Scale control */
+                setRgbColors(_COLOR_SCALE_OFF, _ledPad[extension][12 + _scale]);
+                setRgbColors(_COLOR_SCALE_ON, _ledPad[extension][pad]);
+                setTransModel(pad - 12);
+                return;
+            }
 
         }
         else if (status == OFFSET)
         {
-            setRgbColors(_offColors, _ledPad[extension][pad]);
+            if (pad < 8)
+            {
+                setRgbColors(_offColors, _ledPad[extension][pad]);
+            }
         }
         else if (status == ON)
-        {            
+        {
             // nothing to do in this case
             return;
         }
 
         note(status,
              _midiChannel,
-             _numberExtensions == 1 ? _midiNotes[extension][pad] + _currentPage * LEDPAD_NUM : _midiNotes[extension][pad],
+             _midiNotes[extension][pad],
              _midiVeloc[extension][pad]);
     }
 
@@ -91,81 +263,137 @@ protected:
         uint8_t in_octave = in_note / 12;
         uint8_t in_chroma = in_note % 12;
 
-        _low_bound += _bound_shift[pad];
-        _high_bound += _bound_shift[pad];
-
-        // compute new note
-        float prev_prob = 0;
-        uint8_t out_chroma = 0;
-
+        // Compute the cumulative probalities.
+        uint32_t cumulative_probs[12] = {0};
         for (size_t i = 0; i < 12; i++)
         {
-            float current_prob = _chroma_probs[in_chroma] * _trans_model1[in_chroma][i];
-            if (i != in_chroma)
-            {
-                if (current_prob > prev_prob)
-                {
-                    out_chroma = i;
-                }
-            }
-            
+            cumulative_probs[i] = _chroma_probs[i] * _trans_model[in_chroma][i];
 
-            prev_prob = current_prob;
+            if (i > 0)
+            {
+                cumulative_probs[i] += cumulative_probs[i - 1];
+            }
         }
+
+        // Random sampling.
+        long sample = random(cumulative_probs[11] + 1);
+
+        int min_distance = cumulative_probs[11];
+        int distance = cumulative_probs[11];
+        uint8_t out_chroma = 0;
+
+        // Check which note was attached to the "winner" bin.
+        for (size_t i = 0; i < 12; i++)
+        {
+            distance = cumulative_probs[i] - sample;
+            if ((distance > 0) && (distance < min_distance))
+            {
+                min_distance = distance;
+                out_chroma = i;
+            }
+        }
+
 
         uint8_t out_note = out_chroma + in_octave * 12;
 
-        // center the note in the target range
-
-        if (out_note < _low_bound)
+        // Center the note in the target range
+        // if we wanted a lower note make sure it is a lower note,
+        // other wase octave it
+        if (pad < 4)
         {
-            out_note += 12;
+            if (out_note > in_note)
+            {
+                out_note -= 12;
+            }
         }
 
-        if (out_note > _high_bound)
+        // Same for the higher case.
+        if (pad >= 4)
         {
-            out_note -= 12;
+            if (out_note < in_note)
+            {
+                out_note += 12;
+            }
         }
-        
-        if (out_note > _low_bound) setRgbColors(_DEBUG_RED, _ledPad[extension][pad]);
-        if (out_note < _high_bound) setRgbColors(_DEBUG_RED, _ledPad[extension][pad]);;
 
         // update variables
         _current_note = out_note;
         _current_octave = out_note / 12;
-        _current_chroma = out_note % 12;
 
-        _midiNotes[extension][pad] = _current_note;
+        // Save the note with the current transposition value
+        _midiNotes[extension][pad] = _current_note + _transposition;
+
+        // update octave LEDs
+        updateOctaveLEDs();
+    }
+
+    // octave 5 stands for C4
+    // going up will increasingly light
+    // right octave PAD and goung down
+    // will increasingly light up the
+    // left octave PAD.
+    void updateOctaveLEDs()
+    {
+        if (_current_octave < 5)
+        {
+            uint8_t intensity = (4 - _current_octave) * 63.75f;
+            uint8_t color[3] = {0, 0, intensity};
+            setRgbColors(color, _ledPad[0][9]);
+        }
+        if (_current_octave > 5)
+        {
+            // MIDI messages can go up to octave 11.
+            // Clip the signal so we don't exceed 
+            // uint8 range.
+            uint8_t octave_clipped = min(_current_octave, 9);
+
+            uint8_t intensity = (octave_clipped - 5) * 63.75f;
+            uint8_t color[3] = {0, 0, intensity};
+            setRgbColors(color, _ledPad[0][8]);
+        }
+        if (_current_octave == 5)
+        {
+            uint8_t color[3] = {0, 0, 0};
+            setRgbColors(color, _ledPad[0][8]);
+            setRgbColors(color, _ledPad[0][9]);
+        }
+    }
+
+
+    void updateTranspositionLEDs()
+    {
+        if (_transposition < 0)
+        {
+            uint8_t intensity = -_transposition * 21.25f;
+            uint8_t color[3] = {0, intensity, 0};
+            setRgbColors(color, _ledPad[0][11]);
+        }
+        if (_transposition > 0)
+        {
+            // MIDI messages can go up to octave 11.
+            // Clip the signal so we don't exceed 
+            // uint8 range.
+            uint8_t intensity = _transposition * 21.25f;
+            uint8_t color[3] = {0, intensity, 0};
+            setRgbColors(color, _ledPad[0][10]);
+        }
+        if (_transposition == 0)
+        {
+            uint8_t color[3] = {0, 0, 0};
+            setRgbColors(color, _ledPad[0][10]);
+            setRgbColors(color, _ledPad[0][11]);
+        }
+        
     }
 };
 
 MiDispositivoMIDIAutomelody mdma = MiDispositivoMIDIAutomelody(4, 1);
 
-void setup() {
-  // Serial.begin(9600);  //  debugging output
-
-  // Assign MIDI notes to buttons. Note that if you are using
-  // more than one expansion (more than one 4x4 device) you will
-  // need to change the size of this array. Always 1 value per button.
-  uint8_t midiNotes [1][16] = {{ 30, 34, 50, 33,
-                                100, 35, 36, 37,
-                                120, 39, 40, 41,
-                                 30, 43, 44, 45}};
-  mdma.setMidiNotes(midiNotes);
-
-  // Same with the so called velocities of the notes. This means the
-  // intensity of each note.
-  uint8_t midiVelocities [1][16] = {{30, 30, 30, 30,
-                                    127, 35, 36, 37,
-                                    120, 39, 40, 41,
-                                     30, 43, 44, 45}};
-
-  mdma.setMidiVeloc(midiVelocities);
+void setup()
+{
+    Serial.begin(9600);
 }
 
 void loop() {
-
-  // Run the loop. This will read which buttons are pressed and
-  // execute different actions, such as sending the MIDI messages
   mdma.loop();
 }
